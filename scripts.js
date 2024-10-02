3ae54ebeda023de4961de6d8c2c41f11749d97bf
SulaimanS <95322305+SulaimanS11@users.noreply.github.com>
2024-10-02 01:00:13 -0400

    animations and edits

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typewriter",
+    {
+        strings: [
+            " ",
+            "Full Stack Developer",
+            "Computer Scientist",
+            "Student"
+        ],
+        typeSpeed:60,
+        backSpeed:40,
+        loop:true
+    }
+)
