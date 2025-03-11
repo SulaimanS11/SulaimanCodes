232ac4eb4939de5799ec7397394e34d3cb9ea473
SulaimanS <95322305+SulaimanS11@users.noreply.github.com>
2025-03-11 15:24:07 -0400

    Add toggle theme button to React.js app

---
+++ React.js/src/components/ThemeToggle.js
@@ -0,0 +1,15 @@
+﻿import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+import './ThemeToggle.css';
+
+const ThemeToggle = () => {
+    const { darkMode, toggleTheme } = useContext(ThemeContext);
+
+    return (
+        <div className="theme-toggle" onClick={toggleTheme}>
+            <i className={`bx ${darkMode ? 'bx-sun' : 'bx-moon'}`}></i>
+        </div>
+    );
+};
+
+export default ThemeToggle;
